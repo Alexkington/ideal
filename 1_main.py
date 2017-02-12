41dffd593e55df58342e7aec72372ccb2076f408
Alexkington <960693870@qq.com>
2017-02-12 22:05:06 +0800

    Create 1_main.py

---
+++ 1_main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os,re
+# 列出当前目录下所有的文件
+files = os.listdir(".")
+for filename in files:
+    portion = os.path.splitext(filename)
+    # 如果后缀是.txt
+    if portion[1] == ".vtt":
+        # 重新组合文件名和后缀名files = os.listdir(".")
+        newname = portion[0] + ".txt"
+        os.rename(filename,newname)
+
+files = os.listdir(".")
+for filename in files:
+    portion = os.path.splitext(filename)
+    # 如果后缀是.txt
+    if portion[1] == ".txt":
+        with open(filename,'rt+') as f:
+            date=f.read()
+            date=date.replace('<v Chinese>','')
+            date=date.replace('</v>','')
+            f.seek(0)
+            f.write(date)
+        newname = portion[0] + ".srt"
+        os.rename(filename,newname)
