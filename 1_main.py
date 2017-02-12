95e1d29bd81e7de83d21c37481f3daf24e5ea154
Alexkington <960693870@qq.com>
2017-02-12 22:10:45 +0800

    Update 1_main.py

--- 1_main.py
+++ 1_main.py
@@ -5,7 +5,7 @@ import os,re
 files = os.listdir(".")
 for filename in files:
     portion = os.path.splitext(filename)
-    # 如果后缀是.txt
+    # 如果后缀是.vtt
     if portion[1] == ".vtt":
         # 重新组合文件名和后缀名files = os.listdir(".")
         newname = portion[0] + ".txt"
